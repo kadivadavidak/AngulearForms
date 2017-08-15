dbc76d9c07a0da081419919a4ba838161863d965
David Kirschman <kadivadavidak@gmail.com>
2017-08-15 16:37:03 -0600

    Init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  rForm: FormGroup;
+  post: any;
+  description: string = '';
+  guestName: string = '';
+  titleAlert: string = 'This field is required';
+
+  constructor(private fb: FormBuilder){
+    this.rForm = fb.group({
+      'guestName': [null, Validators.required],
+      'description': [null, Validators.compose([Validators.required, Validators.minLength(30), Validators.maxLength(500)])],
+      'validate': ''
+    });
+  }
+
+  ngOnInit() {
+    this.rForm.get('validate').valueChanges.subscribe(
+      (validate) => {
+        if (validate == '1') {
+          this.rForm.get('guestName').setValidators([Validators.required, Validators.minLength(3)]);
+          this.titleAlert = "You need to specify at least 3 characters";
+        } else {
+          this.rForm.get('guestName').setValidators(Validators.required);
+        }
+        this.rForm.get('guestName').updateValueAndValidity();
+      }
+    )
+  }
+
+  addPost(post){
+    this.description = post.description;
+    this.guestName = post.guestName;
+  }
 }
